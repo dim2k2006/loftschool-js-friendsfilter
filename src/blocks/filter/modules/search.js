869d5828c47705e86e77b2896ed6139d8cbd74ab
Дмитрий Малофеев <dim2k2006@yandex.ru>
2016-08-08 22:30:37 +0300

    Method search implemented

--- src/blocks/filter/modules/search.js
+++ src/blocks/filter/modules/search.js
@@ -1,7 +1,31 @@
 var search = function(event) {
-    var value = event.target.value;
+    var __this = this,
+        targetId = event.target.getAttribute('id'),
+        value = event.target.value,
+        container = (targetId === 'search__input_1') ? __this.listAll : __this.listCustom,
+        template = (targetId === 'search__input_1') ? __this.listAllTemplate : __this.listCustomTemplate,
+        sortData = (targetId === 'search__input_1') ? __this.listAllData : __this.listCustomData;
+        temp = [];
 
-    console.log(value);
+    if (sortData && value !== '') {
+
+        temp = sortData.filter(function(item) {
+
+            if ((item.first_name + ' ' + item.last_name).toLowerCase().indexOf(value.toLowerCase()) !== -1) {
+
+                return item;
+
+            }
+
+        });
+
+        __this.render(container, template, temp);
+
+    } else {
+
+        __this.render(container, template, sortData);
+
+    }
 };
 
 module.exports = search;
