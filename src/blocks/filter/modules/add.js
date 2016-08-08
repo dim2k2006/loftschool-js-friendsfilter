f688dbb3a798e9ffd3f41981f0776c9208c0dd84
Дмитрий Малофеев <dim2k2006@yandex.ru>
2016-08-08 22:05:02 +0300

    Method remove implemented

--- src/blocks/filter/modules/add.js
+++ src/blocks/filter/modules/add.js
@@ -2,16 +2,13 @@ var add = function(event, target) {
     event.preventDefault();
 
     var __this = this,
-        id = parseInt(target.getAttribute('id')),
-        index = '';
+        id = parseInt(target.getAttribute('id'));
 
     __this.listAllData.forEach(function(item, i, arr) {
         if (item.user_id === id) {
 
-            index = __this.listAllData.indexOf(item);
-
             __this.listCustomData.push(item);
-            __this.listAllData.splice(index, 1);
+            __this.listAllData.splice(i, 1);
 
 
             __this.render(__this.listCustom, __this.listCustomTemplate, __this.listCustomData);
--- src/blocks/filter/modules/remove.js
+++ src/blocks/filter/modules/remove.js
@@ -1,7 +1,21 @@
-var remove = function(event) {
+var remove = function(event, target) {
     event.preventDefault();
 
-    console.log('remove');
+    var __this = this,
+        id = parseInt(target.getAttribute('id'));
+
+    __this.listCustomData.forEach(function(item, i, arr) {
+        if (item.user_id === id) {
+
+            __this.listAllData.push(item);
+            __this.listCustomData.splice(i, 1);
+
+            __this.render(__this.listCustom, __this.listCustomTemplate, __this.listCustomData);
+            __this.render(__this.listAll, __this.listAllTemplate, __this.listAllData);
+
+            return;
+        }
+    });
 };
 
 module.exports = remove;
